41125ff20e9841c282e8bc4a4896fd114f1686cd
Fer Hernandez <fh.1111@gmail.com>
2018-12-27 00:13:27 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -8,8 +8,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      t.timestamp :opening_time
-      t.string :ending_time
+      t.datetime :opening_time
+      t.datetime :ending_time
     end
   end
 end
