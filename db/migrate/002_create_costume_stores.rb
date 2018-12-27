b840d5aa51fa3d3a381b6794a1423a9d4796414a
Fer Hernandez <fh.1111@gmail.com>
2018-12-27 00:01:30 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,9 +2,14 @@
 
 class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
-    create_table :customeStores do |t|
+    create_table :costume_stores do |t|
       t.string :name
-      t.string :breed
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :number_of_employees
+      t.boolean :open
+      t.string :opening_time
+      t.string :closing_time
     end
   end
 end
