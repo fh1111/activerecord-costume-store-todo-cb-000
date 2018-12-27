fb9558644495df80ccdd36d65cc55bb16302fca0
Fer Hernandez <fh.1111@gmail.com>
2018-12-27 00:17:12 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "location"
     t.string "theme"
     t.float "price"
-    t.boolean "friendly"
+    t.boolean "family_friendly"
     t.string "opening_date"
     t.string "closing_date"
     t.string "long_description"
